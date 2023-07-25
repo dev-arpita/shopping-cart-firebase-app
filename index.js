790eed2d00f1d65077b7e08190631270a59dc58d
Arpita Roy <arpita.roy.workar@gmail.com>
2023-07-25 13:30:18 +0530

    Adding Firebase to project

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://realtimedb-d3b7c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value;
     
+    // Challenge: Use the Firebase function 'push' to push inputValue to the database
+    push(shoppingListInDB, inputValue);
     console.log(inputValue)
-})
+});
